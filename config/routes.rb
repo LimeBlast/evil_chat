606f48d63e196930a274eee602cc377fcd8fbaa8
Daniel Hollands <daniel@limeblast.co.uk>
2018-09-07 21:36:03 +0100

    Something about logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'chat#show'
+
+  get '/login', to: 'auth#new'
+  post '/login', to: 'auth#create'
 end

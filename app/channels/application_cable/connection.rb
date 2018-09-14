f7ca31467b3b6e5d0279073df7ce7fe3f9fe1451
Daniel Hollands <daniel@limeblast.co.uk>
2018-09-14 14:47:04 +0100

    I don't understand any of this.

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = request.session.fetch('username', nil)
+      reject_unauthorized_connection unless current_user
+    end
   end
 end
